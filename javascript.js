63b5699d8f7816d2174f34fbd94f6d278de8aac5
jonathan-luo <jonathanluo10@gmail.com>
2022-02-22 00:01:16 -0500

    a couple new additions

--- javascript.js
+++ javascript.js
@@ -1,48 +1,48 @@
 /* input JavaScript code here! */
 function startMsg() {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    let name = urlParams.get('name');
-    name = name ? ` <strong class="specialName">${getRevName(name)}</strong>`: "";
-    document.getElementById("welcome").innerHTML = `What's goody${name}, welcome to Bing Bong dot com.`;
+    let name = getName();
+    document.getElementById("welcome").innerHTML = `What's goody${name}, welcome to BingBong.com. Also, please don't touch me.`;
 }
 
 function scared() {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    let name = urlParams.get('name');
-    name = name ? `, <strong class="specialName">${getRevName(name)}</strong>`: "";
+    let name = getName();
     document.getElementById("welcome").innerHTML = `Ah${name}! Please DON'T touch me!`;
 }
 
 function relieved() {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    let name = urlParams.get('name');
-    name = name ? `, <strong class="specialName">${getRevName(name)}</strong>`: "";
-    document.getElementById("welcome").innerHTML = `That's better. Anyways${name}, welcome to Bing Bong dot com.`;
+    let name = getName();
+    document.getElementById("welcome").innerHTML = `That's better. Anyways${name}, welcome to BingBong.com.`;
 }
 
 function spencer() {
-    let n = parseInt(document.getElementById("spencer").innerHTML)
+    let n = parseInt(document.getElementById("spencer").innerHTML);
     document.getElementById("spencer").innerHTML = `${n + 1} -- Keep going, he loves it!`;
 }
 
 function cannoli() {
-    let n = parseInt(document.getElementById("cannoli").innerHTML)
-    let msg = "Aww, Cannoli's a lil' shy, try again to see if you can pet her!"
+    let n = parseInt(document.getElementById("cannoli").innerHTML);
+    let msg;
     if (Math.random() < 0.1) {
         n += 1;
-        msg = "Whoa, you actually pet Cannoli! Congrats!"
+        msg = "Whoa, you actually pet Cannoli! Congrats!";
+    } else {
+        msg = "Aww, Cannoli's a lil' shy, try again to see if you can pet her!";
     }
     document.getElementById("cannoli").innerHTML = `${n} -- ${msg}`;
 }
 
 function scout() {
-    let n = parseInt(document.getElementById("scout").innerHTML)
+    let n = parseInt(document.getElementById("scout").innerHTML);
     document.getElementById("scout").innerHTML = `${n + 1} -- Keep going, he loves it!`;
 }
 
-function getRevName(s) {
+function getName() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    let name = urlParams.get('name');
+    return name ? `, <strong class="specialName">${revString(name)}</strong>`: "";
+}
+
+function revString(s) {
     return s.split("").reverse().join("");
 }
